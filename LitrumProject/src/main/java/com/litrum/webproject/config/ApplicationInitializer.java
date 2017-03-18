e1140db02e0f6a49d0e2d99827bb0acad9867ddc
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-18 17:39:06 +0530

    adde a spring web initialize class.

---
+++ LitrumProject/src/main/java/com/litrum/webproject/config/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.litrum.webproject.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by Pc on 18/03/2017.
+ */
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringServletConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
---
+++ LitrumProject/src/main/java/com/litrum/webproject/config/SpringServletConfig.java
@@ -0,0 +1,48 @@
+package com.litrum.webproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+/**
+ * Created by Pc on 18/03/2017.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.litrum.webproject.controller")
+public class SpringServletConfig extends WebMvcConfigurerAdapter{
+
+    /**
+     * Configure TilesConfigurer.
+     */
+    @Bean
+    public TilesConfigurer tilesConfigurer(){
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[] {"WEB-INF/layouts/views.xml", "/WEB-INF/views/**/views.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    /**
+     * Configure ViewResolvers to deliver preferred views.
+     */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+
+    /**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**", "/resources/**").addResourceLocations("/", "/resources");
+    }
+}
