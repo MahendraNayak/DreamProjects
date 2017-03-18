bb1aeb511b45c07b9da194f427c0b9c2349d2050
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-18 12:55:12 +0530

    added dependency for spring mvc and also added a sample structure of web project

---
+++ LitrumProject/src/main/java/com/litrum/webproject/controller/RegistrationController.java
@@ -0,0 +1,10 @@
+package com.litrum.webproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Pc on 18/03/2017.
+ */
+@Controller
+public class RegistrationController {
+}
