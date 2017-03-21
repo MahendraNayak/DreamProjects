913b3d50860dee573f7ec2df5ec7ee322b39aa41
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-21 22:06:17 +0530

    addea a simple pojo class for binding form value and pass it to server.

--- LitrumProject/src/main/java/com/litrum/webproject/form/RegisterForm.java
+++ LitrumProject/src/main/java/com/litrum/webproject/form/RegisterForm.java
@@ -1,12 +1,42 @@
 package com.litrum.webproject.form;
 
+import com.litrum.webproject.model.CompanyType;
+import com.litrum.webproject.model.EndUserRegistration;
+import com.litrum.webproject.model.EndUserRole;
+import com.litrum.webproject.model.ServiceOffered;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 /**
  * Created by Pc on 18/03/2017.
  */
 public class RegisterForm {
-
+    private String firstName;
+    private String lastName;
     private String userName;
-    private String passWord;
+    private String password;
+    private String mobileNumber;
+    private String emailId;
+    private String companyCity;
+    private String serviceOffered;
+    private String companyType;
+    private String endUserRole;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getUserName() {
         return userName;
@@ -16,19 +46,75 @@ public class RegisterForm {
         this.userName = userName;
     }
 
-    public String getPassWord() {
-        return passWord;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getCompanyCity() {
+        return companyCity;
+    }
+
+    public void setCompanyCity(String companyCity) {
+        this.companyCity = companyCity;
+    }
+
+    public String getServiceOffered() {
+        return serviceOffered;
+    }
+
+    public void setServiceOffered(String serviceOffered) {
+        this.serviceOffered = serviceOffered;
+    }
+
+    public String getCompanyType() {
+        return companyType;
+    }
+
+    public void setCompanyType(String companyType) {
+        this.companyType = companyType;
+    }
+
+    public String getEndUserRole() {
+        return endUserRole;
+    }
+
+    public void setEndUserRole(String endUserRole) {
+        this.endUserRole = endUserRole;
     }
 
     @Override
     public String toString() {
-        return "RegisterForm{" +
-                "userName='" + userName + '\'' +
-                ", passWord='" + passWord + '\'' +
-                '}';
+        return new ToStringBuilder(this)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("userName", userName)
+                .append("password", password)
+                .append("mobileNumber", mobileNumber)
+                .append("emailId", emailId)
+                .append("companyCity", companyCity)
+                .append("serviceOffered", serviceOffered)
+                .append("companyType", companyType)
+                .append("endUserRole", endUserRole)
+                .toString();
     }
 }
