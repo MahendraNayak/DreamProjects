5fbc86de9dc068adafd5c6387ede26b672e939be
MehendraNayak <nayak.mahendra2@gmail.com>
2017-03-23 01:54:20 +0530

    Added for  Admin Pannel GUI

--- LitrumProject/src/main/webContent/JS/custom.js
+++ LitrumProject/src/main/webContent/JS/custom.js
@@ -32,3 +32,19 @@ jQuery().ready(function() {
         return false;
     });
 });
+
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+    });
+
+   // Remove menu for searching
+   $('#search-trigger').click(function () {
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+    });
+});
