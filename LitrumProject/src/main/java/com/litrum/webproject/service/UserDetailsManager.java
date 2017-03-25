03bd0b60b750754c09edf8e2addc6456a47d0c96
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-25 23:30:08 +0530

    comment the code

--- LitrumProject/src/main/java/com/litrum/webproject/service/UserDetailsManager.java
+++ LitrumProject/src/main/java/com/litrum/webproject/service/UserDetailsManager.java
@@ -1,14 +1,12 @@
+/*
 package com.litrum.webproject.service;
 
 import com.litrum.webproject.dao.DAOFactory;
-import com.litrum.webproject.model.CompanyDetails;
-import com.litrum.webproject.model.EndUserRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,9 +16,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+*/
 /**
  * Created by Pc on 24/03/2017.
- */
+ *//*
+
 @Service("userDetailsService")
 public class UserDetailsManager implements UserDetailsService {
 
@@ -29,21 +29,24 @@ public class UserDetailsManager implements UserDetailsService {
     @Autowired
     private DAOFactory daoFactory;
 
-    /**
-     * Locates the user based on the username. In the actual implementation, the search
-     * may possibly be case sensitive, or case insensitive depending on how the
-     * implementation instance is configured. In this case, the <code>UserDetails</code>
-     * object that comes back may have a username that is of a different case than what
-     * was actually requested..
-     *
-     * @param username the username identifying the user whose data is required.
-     * @return a fully populated user record (never <code>null</code>)
-     * @throws UsernameNotFoundException if the user could not be found or the user has no
-     *                                   GrantedAuthority
-     */
+    */
+/**
+ * Locates the user based on the username. In the actual implementation, the search
+ * may possibly be case sensitive, or case insensitive depending on how the
+ * implementation instance is configured. In this case, the <code>UserDetails</code>
+ * object that comes back may have a username that is of a different case than what
+ * was actually requested..
+ *
+ * @param username the username identifying the user whose data is required.
+ * @return a fully populated user record (never <code>null</code>)
+ * @throws UsernameNotFoundException if the user could not be found or the user has no
+ *                                   GrantedAuthority
+ *//*
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        /*EndUserRegistration userInfo = daoFactory.getEndUserRegistrationDAO().findByUserName(username);
+        */
+/*EndUserRegistration userInfo = daoFactory.getEndUserRegistrationDAO().findByUserName(username);
         if (null != userInfo){
             CompanyDetails endUserCompanyDetails = daoFactory.getCompanyDetailsDAO().findByEndUserId(userInfo.getId());
             if (null != endUserCompanyDetails){
@@ -58,7 +61,8 @@ public class UserDetailsManager implements UserDetailsService {
         } else {
             logger.error("End User not found.");
             throw new UsernameNotFoundException("userName not found");
-        }*/
+        }*//*
+
         return null;
     }
 
@@ -70,3 +74,4 @@ public class UserDetailsManager implements UserDetailsService {
         return rolesList;
     }
 }
+*/
