5db4c7a21463d42d34327619b309e3d4f6d993a6
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-25 10:18:56 +0530

    added constant file for project

---
+++ LitrumProject/src/main/java/com/litrum/webproject/Utils/LitrumProjectConstants.java
@@ -0,0 +1,11 @@
+package com.litrum.webproject.Utils;
+
+/**
+ * Created by Pc on 24/03/2017.
+ */
+public interface LitrumProjectConstants {
+
+    String ROLE_PREFIX = "ROLE_";
+    String ROLE_ADMIN = "ADMIN";
+    String ROLE_END_USER = "ENDUSER";
+}
