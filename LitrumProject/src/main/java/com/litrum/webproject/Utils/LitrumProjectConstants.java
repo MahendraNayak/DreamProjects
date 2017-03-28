8f53dd67323fc4ccd78d7d89f43f3b03668d65d7
MahendraNayak <nayak.mahendra2@gmail.com>
2017-03-28 15:43:38 +0530

    added constant

--- LitrumProject/src/main/java/com/litrum/webproject/Utils/LitrumProjectConstants.java
+++ LitrumProject/src/main/java/com/litrum/webproject/Utils/LitrumProjectConstants.java
@@ -6,6 +6,10 @@ package com.litrum.webproject.Utils;
 public interface LitrumProjectConstants {
 
     String ROLE_PREFIX = "ROLE_";
-    String ROLE_ADMIN = "ADMIN";
+    String ROLE_SUPER_ADMIN = "SUPER_ADMIN";
     String ROLE_END_USER = "ENDUSER";
+    String ROLE_EDITOR = "EDITOR";
+    String ROLE_AUTHORISER = "AUTHORISER";
+    String ROLE_PRIORITY_EDITOR = "PRIORITY EDITOR;";
+    String ROLE_SALES = "SALES";
 }
