335ff5148312dc11580043ae864ce84fe2492d73
MahendraNayak <nayak.mahendra2@gmail.com>
2017-04-15 04:18:01 +0530

    added pojo class for add sub main item

---
+++ LitrumProject/src/main/java/com/litrum/webproject/form/SubMainItemsForm.java
@@ -0,0 +1,96 @@
+package com.litrum.webproject.form;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * Created by Pc on 15/04/2017.
+ */
+public class SubMainItemsForm {
+
+    private Long mainItemId;
+    private Long loadUnitId;
+    private String shortDecription;
+    private Long subMainIemId;
+    private Double subMainItemMakerRate;
+    private String subMainItemMakerName;
+    private Double subMainItemContractorRate;
+    private String subMainItemContractorName;
+
+    public Long getMainItemId() {
+        return mainItemId;
+    }
+
+    public void setMainItemId(Long mainItemId) {
+        this.mainItemId = mainItemId;
+    }
+
+    public Long getLoadUnitId() {
+        return loadUnitId;
+    }
+
+    public void setLoadUnitId(Long loadUnitId) {
+        this.loadUnitId = loadUnitId;
+    }
+
+    public String getShortDecription() {
+        return shortDecription;
+    }
+
+    public void setShortDecription(String shortDecription) {
+        this.shortDecription = shortDecription;
+    }
+
+    public Long getSubMainIemId() {
+        return subMainIemId;
+    }
+
+    public void setSubMainIemId(Long subMainIemId) {
+        this.subMainIemId = subMainIemId;
+    }
+
+    public Double getSubMainItemMakerRate() {
+        return subMainItemMakerRate;
+    }
+
+    public void setSubMainItemMakerRate(Double subMainItemMakerRate) {
+        this.subMainItemMakerRate = subMainItemMakerRate;
+    }
+
+    public String getSubMainItemMakerName() {
+        return subMainItemMakerName;
+    }
+
+    public void setSubMainItemMakerName(String subMainItemMakerName) {
+        this.subMainItemMakerName = subMainItemMakerName;
+    }
+
+    public Double getSubMainItemContractorRate() {
+        return subMainItemContractorRate;
+    }
+
+    public void setSubMainItemContractorRate(Double subMainItemContractorRate) {
+        this.subMainItemContractorRate = subMainItemContractorRate;
+    }
+
+    public String getSubMainItemContractorName() {
+        return subMainItemContractorName;
+    }
+
+    public void setSubMainItemContractorName(String subMainItemContractorName) {
+        this.subMainItemContractorName = subMainItemContractorName;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("mainItemId", mainItemId)
+                .append("loadUnitId", loadUnitId)
+                .append("shortDecription", shortDecription)
+                .append("subMainIemId", subMainIemId)
+                .append("subMainItemMakerRate", subMainItemMakerRate)
+                .append("subMainItemMakerName", subMainItemMakerName)
+                .append("subMainItemContractorRate", subMainItemContractorRate)
+                .append("subMainItemContractorName", subMainItemContractorName)
+                .toString();
+    }
+}
